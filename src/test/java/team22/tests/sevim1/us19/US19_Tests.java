89e8df406f391ec31766ac5eeb81b8e9a314789c
Sevimsmgr <sevimsmgr@gmail.com>
2023-02-06 21:18:13 +0100

    v2

--- src/test/java/team22/tests/sevim1/us19/US19_Tests.java
+++ src/test/java/team22/tests/sevim1/us19/US19_Tests.java
@@ -1,2 +1,25 @@
-package team22.tests.sevim1.us19;public class US19_Tests {
+package team22.tests.sevim1.us19;
+
+import org.testng.annotations.Test;
+
+public class US19_Tests {
+
+    //Kullanici anasayfayı açar
+    //Kullanici uygulamaya login olur
+    //My Account sayfasi görüntülenir
+    //Kullanici kullanici Categories kismina hoverover yapar
+    //Kullanici bir category secer ve category'e tiklar
+    //Kullanici bir ürün secer ve ürüne tiklar
+    //Kullanici add to cart butonuna tiklar
+    //Kullanici tekrar Categories kismina hover over yapar
+    //Kullanici bir Category secer ve category'e tiklar
+    //Kullanici ilk ürüne tiklar
+    //Kullanici add to cart butonuna tiklar
+    //Kullanici "cart" butonuna tiklar
+    //Ürünlerin sepette oldugu dogrulanir
+
+    @Test
+    public void TC001_US19(){
+        System.out.println("test 1");
+    }
 }
